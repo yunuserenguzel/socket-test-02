00de89a97c50aed1bef24261f5999fec316d1b30
Yunus Eren Guzel <exculuber@gmail.com>
2014-10-02 14:10:25 +0300

    asdasd

--- SocketTest02/ViewController.m
+++ SocketTest02/ViewController.m
@@ -30,8 +30,9 @@
 
     [self.tableView registerClass:[UITableViewCell class] forCellReuseIdentifier:@"Cell"];
     
-//    [[HttpRequestManager sharedInstance] GET:@"http://yeg-rooms.herokuapp.com/rooms.json" parameters:nil success:^
-     [[HttpRequestManager sharedInstance] GET:@"http://192.168.1.106:3000/rooms.json" parameters:nil success:^(AFHTTPRequestOperation *operation, id responseObject) {
+    [[HttpRequestManager sharedInstance] GET:@"http://yeg-rooms.herokuapp.com/rooms.json" parameters:nil success:
+//     [[HttpRequestManager sharedInstance] GET:@"http://192.168.1.106:3000/rooms.json" parameters:nil success:
+     ^(AFHTTPRequestOperation *operation, id responseObject) {
         NSLog(@"%@",operation.responseObject);
         for (NSDictionary* roomDict in [responseObject objectForKey:@"rooms"]) {
             [self.rooms addObject:[Room roomWithName:[roomDict objectForKey:@"name"]
